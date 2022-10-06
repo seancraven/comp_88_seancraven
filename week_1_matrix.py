33f83d074ee3b5f8690b1805e1b405267b1b324d
seancraven <seanpietercraven@gmail.com>
2022-10-06 08:43:30 +0100

    Finished Decision boundary.

---
+++ week_1_matrix.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+import utils
+matplotlib.use("TkAgg")
+
+def matrix_class(limits=(-5,5), weights=np.random.random((3,3)), num_samples=1000):
+    X = np.ones((num_samples, 3))
+    X[:,:2] = utils.make_random(num_samples,limits, count=2)
+    y =[]
+    for x in X.T:
+        intermediate_prod = np.matmul(weights,x.T)
+        y.append(np.matmul(x,intermediate_prod))
+if __name__ == "__main__":
+    matrix_class()\

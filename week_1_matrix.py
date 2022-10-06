6d6a067ed5245b77aa410fe60629434908efda51
seancraven <seanpietercraven@gmail.com>
2022-10-06 08:44:43 +0100

    Penultimate extension question

--- week_1_matrix.py
+++ week_1_matrix.py
@@ -9,6 +9,7 @@ def matrix_class(limits=(-5,5), weights=np.random.random((3,3)), num_samples=100
     X[:,:2] = utils.make_random(num_samples,limits, count=2)
     y =[]
     for x in X.T:
+        #find solution to doing these mat prods.
         intermediate_prod = np.matmul(weights,x.T)
         y.append(np.matmul(x,intermediate_prod))
 if __name__ == "__main__":
